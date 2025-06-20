e4dae9a4b12afc5df89b459b9217ae55722e5333
MU AHMED <lifeofcoder19@gmail.com>
2025-06-21 04:54:16 +0530

    Enhance Home page layout by adding FAQ and Feature components; adjust padding for improved visual consistency.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,27 +3,26 @@ import { useState } from 'react';
 import FloatingButton from '@/app/components/ui/ChatWidget/FloatingButton';
 import ChatModal from '@/app/components/ui/ChatWidget/ChatModal';
 import WaitingTime from '@/app/components/Waiting-Time/Page';
+import FAQ from './components/FAQ/Page';
+import Feature from './components/Feature/Page';
 
 export default function Home() {
-  const [isChatOpen, setIsChatOpen] = useState(false); //added
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  
   return (  
     <>
-    
-      <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-8 pt-32 sm:pt-40 md:pt-48 pb-8">
-        <div className="relative w-full bg-transparent flex items-center justify-center overflow-hidden">
-          
+      <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-8 pt-8 sm:pt-12 md:pt-16 pb-8">
+        <div className="w-full flex items-center justify-center">
+          <Feature />
         </div>
         <div className="w-full flex items-center justify-center">
-        
-        </div>
-        <div className="w-full flex items-center justify-center">
-        
+          <FAQ/>
         </div>
         <div className="w-full flex items-center justify-center">
           <WaitingTime/>
         </div>
       </div>
-       <FloatingButton onClick={() => setIsChatOpen(true)} />
+      <FloatingButton onClick={() => setIsChatOpen(true)} />
       <ChatModal isOpen={isChatOpen} onClose={() => setIsChatOpen(false)} />
     </>
   );
